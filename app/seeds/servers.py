ebba2480699cbadf16f92bd82957e598e18e6fbc
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-12-14 22:38:57 -0600

    channel seeder files created + seeded to db

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -4,7 +4,7 @@ from random import randint
 
 fake = Faker()
 
-# Adds a demo user, you can add other users here if you want
+
 def seed_servers():
    demo_server = Server(
       owner_id=1,
@@ -36,11 +36,7 @@ def seed_servers():
 
    db.session.commit()
 
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and RESET IDENTITY
-# resets the auto incrementing primary key, CASCADE deletes any
-# dependent entities
+
 def undo_servers():
    db.session.execute('TRUNCATE servers RESTART IDENTITY CASCADE;')
    db.session.commit()
