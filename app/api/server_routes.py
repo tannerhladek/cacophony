bf44ee1f5f2459bc3b7d4d7673d14ef5e7458cb8
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-12-16 16:53:02 -0600

    create server backend route WIP

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,12 +1,49 @@
-from flask import Blueprint
-from flask_login import login_user, current_user
-from flask_login.utils import login_required
-from app.models import Server
+from flask import Blueprint, request
+from flask_login import login_required, current_user
+from app.models import db, Server
+from app.forms import CreateServerForm
 
 server_routes = Blueprint('servers', __name__)
 
+def validation_errors_to_error_messages(validation_errors):
+   """
+   Simple function that turns the WTForms validation errors into a simple list
+   """
+   errorMessages = []
+   for field in validation_errors:
+      for error in validation_errors[field]:
+         errorMessages.append(f'{field} : {error}')
+   return errorMessages
 
-@server_routes.route('/new')
-# @login_required
+
+# get all servers route
+#  TO DO: DELETE THIS ROUTE ---- FOR TESTING ONLY
+@server_routes.route('/')
+def getAllServers():
+   servers = Server.query.all()
+   return {server.to_dict()['id']: server.to_dict() for server in servers}
+
+
+# create new server route
+@server_routes.route('/new', methods =['POST'])
+# @ login_required
 def createServer():
-   return 'you hit the create server route!'
+   form = CreateServerForm()
+   form['csrf_token'].data = request.cookies['csrf_token']
+   if form.validate_on_submit():
+      if form.data['server_image_url']:
+         new_server = Server(
+            name = form.data['name'],
+            server_image_url = form.data['server_image_url'],
+            owner_id = '1'
+         )
+      else:
+         new_server = Server(
+            name = form.data['name'],
+            owner_id = '1'
+         )
+      db.session.add(new_server)
+      db.session.commit()
+      return new_server.to_dict()
+   else:
+      return {'errors': validation_errors_to_error_messages(form.errors)}, 401
--- app/forms/create_server_form.py
+++ app/forms/create_server_form.py
@@ -1,29 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, ValidationError, URL, Optional
 from app.models import Server
 
 
-# def user_exists(form, field):
-#     # Checking if user exists
-#     email = field.data
-#     user = User.query.filter(User.email == email).first()
-#     if not user:
-#         raise ValidationError('Email provided not found.')
+def servername_exists(form, field):
+   # Checking if server_name already exists
+   server_name = field.data
+   server = Server.query.filter(Server.name == server_name).first()
+   if server:
+      raise ValidationError('Server name is already in use.')
 
 
-# def password_matches(form, field):
-#     # Checking if password matches
-#     password = field.data
-#     email = form.data['email']
-#     user = User.query.filter(User.email == email).first()
-#     if not user:
-#         raise ValidationError('No such user exists.')
-#     if not user.check_password(password):
-#         raise ValidationError('Password was incorrect.')
+# def server_image_url_in_form(form, field):
+#    #checking if server_image_url was entered
+#    server_image_url = field.data
+#    if not server_image_url:
+
 
 
 class CreateServerForm(FlaskForm):
-   email = StringField('email', validators=[DataRequired()])
-   password = StringField('password', validators=[
-                           DataRequired()])
+   name = StringField('name', validators=[DataRequired(), servername_exists])
+   server_image_url = StringField('server_image_url', validators=[URL(require_tld=True, message='Please provide a valid image URL'), Optional()])
