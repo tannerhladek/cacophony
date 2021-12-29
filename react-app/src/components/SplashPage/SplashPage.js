38c3607961b9dfc7745b750561ef61e91bfe3e51
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-12-29 14:25:35 -0600

    splashpage styling updated

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -13,6 +13,14 @@ const SplashPage = () => {
          <div className='splash-img-container'>
             <img className='splash-img' src={SplashImage} alt='splash' />
          </div>
+         <div className='splash-content-container'>
+            <span id='splash-tagline'>
+               IMAGINE A PLACE...
+            </span>
+            <div id='splash-phrase'>
+               ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
+            </div>
+         </div>
          <div id='external-links-container'>
             <SplashFooterBar />
          </div>
