d1144f9c74c724e12c35860b23aaebc986dea8c9
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-12-29 11:28:09 -0600

    basic styling updated for splash-page

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
 // component import
 import SplashNavBar from './SplashNavBar';
+import SplashImage from './assets/Chat_12.png'
 
 //import styles
 import './SplashPage.css'
 
 const SplashPage = () => {
    return (
-      <div>
+      <div id='splash-page-container'>
          <SplashNavBar />
+         <div className='splash-img-container'>
+            <img className='splash-img' src={SplashImage} alt='splash' />
+         </div>
       </div>
    );
 }
